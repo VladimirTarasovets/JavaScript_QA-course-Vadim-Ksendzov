1a4587d557be9464c64ebb7c04237aa9cb104d62
VladimirTarasovets <tarasovets21@gmail.com>
2022-05-12 21:17:30 +0300

    update task2.js

--- HomeWork_4/task2.js
+++ HomeWork_4/task2.js
@@ -1,18 +1,32 @@
 /* Дан массив объектов. Каждый объект является идентификационной карточкой человека. 
 Нам нужно хранить только уникальные значения в этом массиве. 
 Реализуйте функцию, которая будет выполнять эту работу. */
+const fil = require('./task2.json'); 
 
-const fs = require('./task2.json'); 
-
-for (let i = 0; i < fs.length; i++){
-    for (let j = 0; j < fs.length; j++){
+for (let i = 0; i < fil.length; i++){
+    for (let j = 0; j < fil.length; j++){
         if(i == j){
             continue;
         }
-        else if(fs[i].name == fs[j].name)
+        else if(fil[i].name == fil[j].name)
         {
-            fs.splice(j, 1);
+            fil.splice(j, 1);
         }
     }
 }
-fs.forEach(element => console.log(element.name));
+fil.forEach(element => console.log(element.name));
+//----------------------------------------------------------------------------------------------------------
+
+const fs = require('fs')
+const users = JSON.parse(fs.readFileSync("./task2.json"));
+let uniquePersons = []
+users.forEach((el => {
+    if (uniquePersons.indexOf(JSON.stringify(el)) === -1) {
+        uniquePersons.push(JSON.stringify(el));
+    }
+}));
+console.log(uniquePersons)
+//-----------------------------------------------------------------------------------------------------------
+
+let chekid = Array.from(new Set(users.map(item => JSON.stringify(item)))).map(item => JSON.parse(item));
+console.log(chekid)
